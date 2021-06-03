8641bc86111e0a05f0338a1260a65a252f6c03fc
Maede-Dev <maedekarimi69110@gmail.com>
2021-06-03 13:12:39 +0430

    first commit

--- src/tasks.js
+++ src/tasks.js
@@ -1,19 +1,7 @@
 import React from 'react';
 import "./App.js";
 
-// const TaskList = (props) => {
-//  return <div className={"list"}>
-//     {props.tasks.map((item) => (
-//       <TaskItem item={item}/>
-//     ))}
-//   </div>;
-// };
 
-// const TaskItem = (props =>{
-//    return <div className={"task"}>
-//     <p className={"taskTitle"}>{props.item}</p>
-//   </div>
-// })
 
 
 const TaskList = ({tasks}) => {
