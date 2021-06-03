7ffc676d4f54703c2279b0de84d9e8aaefdc394e
Maede-Dev <maedekarimi69110@gmail.com>
2021-06-03 16:19:02 +0430

    test the git

--- src/tasks.js
+++ src/tasks.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React from "react";
 import "./App.js";
 
+const TaskList = ({ tasks }) => {
+  return (
+    <div className={"list"}>
+      {tasks.map((item) => (
+        <TaskItem item={item} />
+      ))}
+    </div>
+  );
+};
 
+//  const TaskItem = ({item}) =>{
+//     return <div className={"task"}>
+//      <p className={"taskTitle"}>{item}</p>
+//    </div>
+//  }
 
-
-const TaskList = ({tasks}) => {
-    return <div className={"list"}>
-       {tasks.map((item) => (
-         <TaskItem item={item}/>
-       ))}
-     </div>;
-   };
-   
-   const TaskItem = ({item}) =>{
-      return <div className={"task"}>
-       <p className={"taskTitle"}>{item}</p>
-     </div>
-   }
-
+const TaskItem = ({ item }) => {
+  return (
+    <div className={"task"}>
+      <p className={"taskTitle"}>{item}</p>
+    </div>
+  );
+};
 export default TaskList;
