59b8c1fa456d469b79371499cb6cd4936d1a1282
محمد حسین جوادی نسب <m.javadi@dcrm.ahanonline>
2021-06-08 16:46:17 +0430

    fix the conflig changes

--- src/App.js
+++ src/App.js
@@ -7,47 +7,23 @@ import AboutUs from "./pages/aboutUs/AboutUs";
 import FooterComponent from "./Components/Footer/FooterComponent";
 import Header from "./Components/Header/Header";
 import Home from "./pages/Home/Home";
-<<<<<<< HEAD
 import ProductList from "./pages/ProductList/ProductList";
 import React from "react";
-=======
-import AboutUs from "./pages/aboutUs/AboutUs";
-import React, { useEffect } from "react";
->>>>>>> 2f2d3f7bd89d8871520a89f577445d61e0df799f
 
 function App() {
-  
   return (
     <>
-<<<<<<< HEAD
-      <Header /> 
-      <div className="main-container">
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} /> 
-            <Route exact path="/aboutUs" component={AboutUs} />
-          </Switch>
-        </Router>
-      </div>
-      <FooterComponent />
-=======
       <Router>
         <Header />
-        <Switch>
-          <div className="main-container">
-            <Route exact path="/aboutUs">
-              <AboutUs />
-            </Route>
-            <Route exact path="/productList">
-              <ProductList />
-            </Route>
-            <Route exact path="/">
-              <Home />
-            </Route>
-          </div>
-        </Switch>
+        <div className="main-container">
+          <Switch>
+            <Route exact path="/aboutUs" component={AboutUs} />
+            <Route exact path="/productList" component={ProductList} />
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </div>
+        <FooterComponent />
       </Router>
->>>>>>> 62515f31ea8e06b55026d38de69cf8e14cf30c51
     </>
   );
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -11,11 +11,7 @@ const Home = () => {
         <div className="home-starter-bg"></div>
         <Row className="home-starter-content">
           <Col span={24} style={{ marginBottom: "20px" }}>
-<<<<<<< HEAD
             <h2 style={{ color: "white" }}>به بوتیک کفشدوزک خوش آمدید</h2>
-=======
-            <h2 style={{ color: "white" }}>به بوتیک کفش دوزک خوش آمدید</h2>
->>>>>>> 62515f31ea8e06b55026d38de69cf8e14cf30c51
           </Col>
           <Col span={24}>
             شما میتوانید هر جنس ، مدل ، سایز و رنگ کفشی را در کفش دوزک شوز تهیه
